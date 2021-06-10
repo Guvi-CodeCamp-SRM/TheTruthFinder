db7cd2f58dbf405842fc02cc11c1f478b430e22f
Sumrit Grover <sumritgrover1@gmail.com>
2021-06-10 20:55:46 +0530

    Minor Patch

--- src/screens/ResultScreen/ResultScreen.js
+++ src/screens/ResultScreen/ResultScreen.js
@@ -6,32 +6,33 @@ import {
   useWindowDimensions,
   TextInput,
   TouchableOpacity,
+  Linking,
 } from 'react-native';
 import Store from '../Store';
 import Axios from 'axios';
 import style from './ResultStyles';
 import LoadingScreen from '../Loader/LoadingScreen';
-import Config from 'react-native-config'
+import Config from 'react-native-config';
 
 export default ResultScreen = (props) => {
   console.log(Store.val);
   const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
-    console.log(Config.RESULT_API,Store.input);
+    console.log(Config.RESULT_API, Store.input);
     Axios({
       method: 'post',
-      url:Config.RESULT_API,
+      url: Config.RESULT_API,
       data: {
         link: Store.input,
       },
     })
       .then((res) => {
         Store.setResult(res.data.results);
-        Store.setHeading(res.data.heading)
+        Store.setHeading(res.data.heading);
         setIsLoading(false);
       })
       .catch((err) => {
-        console.log('err',err);
+        console.log('err', err);
       });
   }, []);
 
@@ -39,9 +40,7 @@ export default ResultScreen = (props) => {
   const width = useWindowDimensions().width;
 
   if (isLoading) {
-    return(
-      <LoadingScreen/>
-    )
+    return <LoadingScreen />;
   } else {
     return (
       <View style={style.background}>
@@ -64,21 +63,29 @@ export default ResultScreen = (props) => {
                 paddingLeft: 15,
                 fontSize: 20,
               }}
-              placeholder={Store.Heading.trimLeft()}
+              placeholder={
+                Store.Heading
+                  ? `${Store.Heading.trimLeft()}`
+                  : `Heading not found`
+              }
               editable={false}
             />
           </View>
         </View>
         <View style={{flex: 10}}>
-          <Text style={style.mainhead}>{`${Store.Heading.trimLeft()}`}</Text>
+          {Store.Heading ? (
+            <Text style={style.mainhead}>{`${Store.Heading.trimLeft()}`}</Text>
+          ) : (
+            <Text>Heading not found</Text>
+          )}
           <Text
             style={{
               color: '#DA88FF',
               fontSize: 30,
               fontWeight: '700',
               margin: 10,
-              textAlign:"center",
-              paddingTop:10
+              textAlign: 'center',
+              paddingTop: 10,
             }}>
             The news entered is {`${Store.Result}`}% authenthic
           </Text>
@@ -88,6 +95,7 @@ export default ResultScreen = (props) => {
             onPress={() => {
               // ! needed to be checked by hari
               // props.navigation.navigate('InfoScreen');
+              Linking.openURL(Store.input);
             }}>
             <Text
               style={{
